38d40f4230f6db99c9e6de097b2a5f09624a1b2e
lê trần khánh duy <jin21cm2k4@gmail.com>
2024-07-12 23:34:21 +0700

    ”ktra”

---
+++ Lesson12/Controllers/LTKDHomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson12.Controllers
+{
+    public class LTKDHomeController : Controller
+    {
+        public ActionResult LTKDIndex()
+        {
+            return View();
+        }
+
+        public ActionResult LTKDAbout()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult LTKDContact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
